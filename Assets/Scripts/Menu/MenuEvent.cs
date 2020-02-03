742cda165dd18512e523d6b59b5d41505d58b29e
RVD246 <vishnuraveendran246@gmail.com>
2020-02-03 22:20:15 +0400

    Menu System mechanic Complete, need to add all the ui elements

---
+++ Assets/Scripts/Menu/MenuEvent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuEvent : MonoBehaviour
+{
+    private static List<MenuEventListener> menuEventList;
+
+    void Awake()
+    {
+        menuEventList = new List<MenuEventListener>();
+    }
+
+    public void Call()
+    {
+        foreach(MenuEventListener listener in menuEventList)
+        {
+            listener.OnCallEvent();
+            Debug.Log("CALLING!!!");
+        }
+    }
+
+    static public void AddListener(MenuEventListener listener)
+    {
+        menuEventList.Add(listener);
+        Debug.Log("Added Listener");
+    }
+
+    static public bool CheckListener(MenuEventListener listener)
+    {
+        foreach (var item in menuEventList)
+        {
+            if (item == listener)
+            {
+                Debug.Log("Listener is not there");
+                return false;
+            }
+        }
+        Debug.Log("Listener is there");
+        return true;
+    }
+}
