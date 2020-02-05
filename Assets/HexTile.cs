3acd6cb9e10640f793a32b6fa9a2fbb261152cc9
pretzeldknight <s.velugula@student.saedubai.com>
2020-02-05 14:44:38 +0400

    Final Master Commit

---
+++ Assets/HexTile.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexTile : MonoBehaviour
+{
+    [HideInInspector] public float hCost = 0;
+    [HideInInspector] public int gCost = 0;
+    [HideInInspector] public int fCost = 0;
+
+    List<HexTile> neighbours = new List<HexTile>();
+
+    bool walkable = true;
+
+    HexTile parent;
+
+    public LayerMask layersToCheck;
+
+    private void Start()
+    {
+        CheckAbove();
+        FindNeighbours();
+    }
+
+    void FindNeighbours()
+    {
+        List<Vector3> dir = HexGrid.instance.Directions(this.transform); 
+
+        for (int i = 0; i < 6; i++)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, dir[i], out hit, 0.5f, layersToCheck))
+            {
+                if (hit.collider.tag == "Tile")
+                {
+                    neighbours.Add(hit.collider.gameObject.GetComponent<HexTile>());
+                    walkable = true;
+                }
+            }
+        }
+    }
+
+    public HexTile Parent
+    {
+        get
+        {
+            return parent;
+        }
+        set
+        {
+            parent = value;
+        }
+    }
+
+    public List<HexTile> returnNeighbours()
+    {
+        return neighbours;
+    }
+
+    public void CalculateFCost()
+    {
+        fCost = (int)hCost + gCost;
+    }
+
+    void CheckAbove()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.up, out hit, 0.5f))
+        {
+            if (hit.collider.tag == "Enviro")
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Pathfinder.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    public static Pathfinder instance = null;
+
+    List<HexTile> openList;
+    List<HexTile> closedList;
+    
+    private const int moveCost = 10;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    List<HexTile> FindPath(HexTile start, HexTile destination)
+    {
+        openList = new List<HexTile>();
+        closedList = new List<HexTile> { start };
+
+        for (int i = 0; i < tile.returnNeighbours().Count; i++)
+        {
+            for (int j = 0; j < tile.returnNeighbours().Count; j++)
+            {
+                tile.gCost = int.MaxValue;
+                tile.CalculateFCost();
+                tile.Parent = null;
+            }
+        }
+
+        start.gCost = 0;
+        start.hCost = CalculateDistance(start, destination);
+        start.CalculateFCost();
+
+        while(openList.Count > 0)
+        {
+            HexTile currentTile = GetLowestFTile(openList);
+            if (currentTile = destination)
+                return CalculatePath(destination); //Reached end
+
+            openList.Remove(currentTile);
+            closedList.Add(currentTile);
+        }               
+
+        return null;
+    }
+
+    List<HexTile> CalculatePath(HexTile tile)
+    {
+        return null;
+    }
+
+    float CalculateDistance(HexTile a, HexTile b)
+    {
+        float xDist = Mathf.Abs(a.transform.position.x - b.transform.position.x);
+        float yDist = Mathf.Abs(a.transform.position.z - b.transform.position.z);
+        float remainDist = Mathf.Abs(xDist - yDist);
+        return moveCost*remainDist;
+    }
+
+    HexTile GetLowestFTile(List<HexTile> list)
+    {
+        HexTile lowestf = tile.returnNeighbours()[0];
+        for(int i = 1; i< tile.returnNeighbours().Count; i++)
+        {
+            if (tile.returnNeighbours()[i].fCost < lowestf.fCost)
+                lowestf = tile.returnNeighbours()[i];
+        }
+        return lowestf;
+    }
+}
+
