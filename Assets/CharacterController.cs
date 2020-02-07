f1a73340242729b5e25d49015fcfa9a0d0bd612e
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2020-02-07 18:56:27 +0400

    Movement and Camera
    finished the movement and camera follow functionality on a test scene

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+
+    Vector3 forward, right;
+
+    Rigidbody rb;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+            Move();
+    }
+
+    private void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
+        Vector3 rightMove = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        Vector3 forwardMove = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+
+        Vector3 unison = Vector3.Normalize(rightMove + forwardMove);
+
+        transform.forward = unison;
+        transform.position += rightMove;
+        transform.position += forwardMove;
+    }
+}
