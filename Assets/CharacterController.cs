b6a89dd0ec30e02ddfedb0dbeba62b529ebc1616
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2020-02-18 18:21:04 +0400

    rb.velocity line is added back also

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -34,6 +34,8 @@ public class CharacterController : MonoBehaviour
 
     public void Move()
     {
+        rb.velocity = Vector3.zero;
+
         Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
         Vector3 rightMove = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
         Vector3 forwardMove = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
