8309a03d97d50355060d66d60b4cee1703113a71
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2020-02-19 11:17:51 +0400

    Removed quartic curve

--- Assets/Resources/Scripts/BezierScript.cs
+++ Assets/Resources/Scripts/BezierScript.cs
@@ -18,8 +18,8 @@ public class BezierScript : MonoBehaviour
         //Bezier formula for quadratic curve = (1−t)^2.P0 + 2t.(1−t).P1 + t^2.P2
 
         //Bezier formula for cubic curve = (1−t)^3.P0 + 3t.(1−t)^2.P1 + 3.(1-t).t^2.P2 + t^3.P3
-        
-        //Bezier formula for quartic curve = (1−t)^5.P0 + 5.t.(1-t)^4.P1 + 10.t^2.(1-t)^3.P2 + 10t^3.(1-t)^2.P3 +
+
+        //Bezier formula for quartic curve = (1-t)^4.P0 + 4.t.(1-t)^3.P1 + 6.t^2.(1-t)^2.P2 + 4.t^3.(1-t)^3.P3 +t^4.P4
 
         time = player.distance;
 
