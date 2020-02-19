c6a4c478e8cae9ebd57ae4403a11ec218983099d
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2020-02-19 09:29:59 +0400

    Bezier Curves implemented in another scene
    will add to camera now

---
+++ Assets/BezierPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierPlayer : MonoBehaviour
+{
+    public Transform start, end;
+
+    [Range(0,1)] public float distance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(start.position, end.position, distance);
+    }
+}
---
+++ Assets/Resources/Scripts/BezierScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierScript : MonoBehaviour
+{
+    public BezierPlayer player;
+    
+    public Transform p0, p1, p2 ,p3;
+
+    [Range(0,1)]
+    public float time;
+
+    private void Update()
+    {
+        //Bezier formula for quadratic curve = (1−t)^2.P0 + 2t.(1−t).P1 + t^2.P2
+
+        time = player.distance;
+
+        transform.position = Mathf.Pow((1 - time), 3) * p0.position + 3 * time * Mathf.Pow((1 - time), 2) * p1.position + 3 * time * time *(1 - time) * p2.position + Mathf.Pow(time, 3) * p3.position;
+    }
+}
