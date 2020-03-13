dd121cc1ac45da0677136fc860b2e524056c04b8
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2020-03-13 20:41:46 +0400

    Adding skills and template classes

---
+++ Assets/Resources/Scripts/Character/Protagonist.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Protagonist : Character
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    public override void Move(HexTile tile)
+    {
+        StartCoroutine(MoveDownPath(Pathfinder.instance.FindPath(GetCurrentTile(), tile)));
+        energy.runTimeValue -= tile.energyCost;
+    }
+
+    public override void Attack(Character target)
+    {
+        Debug.Log("I am attacking the " + target.tag + "!");
+        energy.runTimeValue -= AttackEnergy();
+    }
+
+    public override void SkillOne(HexTile tile)
+    {
+        Debug.Log("I am using Skill1!");
+    }
+
+    public override void SkillTwo(HexTile tile)
+    {
+        Debug.Log("I am using Skill2!");
+    }
+
+    public override int MoveEnergy()
+    {
+        return 2;
+    }
+
+    public override int AttackEnergy()
+    {
+        return 1;
+    }
+
+    public override int Skill1Energy()
+    {
+        return 3;
+    }
+
+    public override int Skill2Energy()
+    {
+        return 4;
+    }
+}
