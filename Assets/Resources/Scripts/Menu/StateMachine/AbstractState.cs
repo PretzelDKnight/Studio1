43948618ad5c55c30265f41697d5436c32cf77de
pretzeldknight <s.velugula@student.saedubai.com>
2020-03-23 13:08:30 +0400

    Commit

---
+++ Assets/Resources/Scripts/Menu/StateMachine/AbstractState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractState
+{
+    protected Agent agent;
+
+    public AbstractState nextState;
+
+    public void AssignAgent(Agent a)
+    {
+        agent = a;
+    }
+
+    public abstract void Function();
+
+    public abstract bool CheckTransitions();
+
+    public virtual void MoveTo(Transform transform)
+    {
+        agent.transform.position = Vector3.MoveTowards(agent.transform.position, transform.position, agent.speed);
+        agent.charge -= Time.deltaTime;
+    }
+}
---
+++ Assets/Resources/Scripts/Menu/StateMachine/Agent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    public float charge = 0;
+    public float MaxCharge = 100;
+    public float MinCharge = 30;
+    public float chargeSpeed = 1;
+
+    public float speed = 10;
+
+    public AbstractState currentState;
+
+    public List<Transform> patrolPoints;
+
+    public Transform rechargePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextState(new Recharge());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState.Function();
+    }
+
+    public void NextState(AbstractState state)
+    {
+        currentState = state;
+        currentState.AssignAgent(this);
+    }
+}
