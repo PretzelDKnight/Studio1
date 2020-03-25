6a542fd8f2dc6d0a7ffd302ec2ac5695aeffd1e0
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2020-03-25 10:39:45 +0400

    Almost done making the battle work

--- Assets/UITemp.cs
+++ Assets/UITemp.cs
@@ -5,40 +5,19 @@ using UnityEngine;
 public class UITemp : MonoBehaviour
 {
     public GameObject storyTrigger;
-    public GameObject battleTrigger;
 
     // Start is called before the first frame update
     void Start()
     {
         storyTrigger.SetActive(false);
-        battleTrigger.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "StoryTrigger")
+        if (other.tag == "StoryTrigger")
         {
             Debug.Log(other.gameObject.tag);
-            storyTrigger.SetActive(true);
-        }
-        if (other.gameObject.tag == "BattleTrigger")
-        {
-            Debug.Log(other.gameObject.tag);
-            battleTrigger.SetActive(true);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "StoryTrigger")
-        {
-            Debug.Log(other.gameObject.tag);
-            storyTrigger.SetActive(false);
-        }
-        if (other.gameObject.tag == "BattleTrigger")
-        {
-            Debug.Log(other.gameObject.tag);
-            battleTrigger.SetActive(false);
+            StorySystem.instance.PlayStory();
         }
     }
 }
