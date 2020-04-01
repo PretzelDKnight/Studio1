2b49211afc5baa48da59e61529ce8b0a88aa02af
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2020-04-01 21:11:03 +0400

    Removed some nodes

--- Assets/Resources/Scripts/AI/AI Trees/AITree.cs
+++ Assets/Resources/Scripts/AI/AI Trees/AITree.cs
@@ -12,14 +12,14 @@ public class AITree : MonoBehaviour
     void Start()
     {
         instance = this;
-        rootNode = new LSelector(new List<AITreeNode> { new Looper(new Sequence(new List<AITreeNode> { new HasEnergy(), new Selector(new List<AITreeNode> {
+        rootNode = new Selector(new List<AITreeNode> { new Sequence(new List<AITreeNode> { new HasEnergy(), new Selector(new List<AITreeNode> {
             new AttackMostFatal(new Selector(new List<AITreeNode> { new HasS2Energy(new InS2Range(new AIS2())), new HasS1Energy(new InS1Range(new AIS1())),
                 new HasBAEnergy(new InBARange(new AIBasic())), new HasMoveBAEnergy(new MoveBA()) })), new AttackLowest(new Selector(new List<AITreeNode>
                 { new HasS2Energy(new InS2Range(new AIS2())), new HasS1Energy(new InS1Range(new AIS1())), new HasBAEnergy(new InBARange(new AIBasic())),
                     new HasMoveBAEnergy(new MoveBA()) })), new AttackNearest(new Selector(new List<AITreeNode> { new HasS2Energy(new InS2Range(new AIS2())),
-                        new HasS1Energy(new InS1Range(new AIS1())), new HasBAEnergy(new InBARange(new AIBasic())), new HasMoveBAEnergy(new MoveBA()) })) }) })), new DoNothing() });
+                        new HasS1Energy(new InS1Range(new AIS1())), new HasBAEnergy(new InBARange(new AIBasic())), new HasMoveBAEnergy(new MoveBA()) })) }) }), new DoNothing() });
     }
-
+    
     public void Execute()
     {
         rootNode.Execute();
