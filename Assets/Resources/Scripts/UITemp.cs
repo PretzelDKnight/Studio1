d0c10c52cadb663280ebe51b2077560ea1d9e5a0
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2020-04-18 19:22:46 +0400

    Added collider for final battle and repositioned battle object

--- Assets/Resources/Scripts/UITemp.cs
+++ Assets/Resources/Scripts/UITemp.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9388b99514ac70a282f5672fab43715c4917563f29602da72f45b4ffb2e44773
-size 1899
+oid sha256:5919f7e98cf783cc8d476456f8d865f46eacb3ed680b9f294d3e1ffa7a5dcb3a
+size 1964
