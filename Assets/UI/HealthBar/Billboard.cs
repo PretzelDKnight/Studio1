d8bd020bce1f1957e36d9ff078c41e634434b3bb
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2020-04-18 07:35:54 +0400

    Health Bar added

---
+++ Assets/UI/HealthBar/Billboard.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6c20b76b4ddedf7509dcfeb1d9ff09013003710b91707c73759a30f3876843ac
+size 238
---
+++ Assets/UI/HealthBar/HealthBar.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:55d7aab126eb36b54704fcb9a24118d421ed6e2cd419e67d1d1227827f3d9d69
+size 564
