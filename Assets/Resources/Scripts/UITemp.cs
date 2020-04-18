409276f24f31164982c37bfa271a68b91c671219
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2020-04-18 19:26:44 +0400

    Small stupid fix

--- Assets/Resources/Scripts/UITemp.cs
+++ Assets/Resources/Scripts/UITemp.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5919f7e98cf783cc8d476456f8d865f46eacb3ed680b9f294d3e1ffa7a5dcb3a
+oid sha256:e1bc423c1f0365ecb2d9ed2eaff3c42e0f25dff191ac440b3cac3f06a980ac2f
 size 1964
