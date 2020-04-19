af7b1e92c259c85471436ab4f594f1e8c7ce4352
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2020-04-19 10:54:11 +0400

    Added Editor Tools for object grouping and renaming

---
+++ Assets/Editor_Tools/Group_Tool/Editor/GroupToolMenu.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cd0b21892a814f8f978d94770950013be4f508272406bcf3d920065498b1cc84
+size 312
---
+++ Assets/Editor_Tools/Rename_Tool/Editor/Rename_Tool.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b92c33d1d06b504b7cc2f5fa2b30d7ce69fe55ee8d6058778d7bc8409bb8abe9
+size 1815
