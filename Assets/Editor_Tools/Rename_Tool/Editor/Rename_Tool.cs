b9a3be3eb0a1eec9e69c40721b98590e44dad253
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2020-04-21 14:27:11 +0400

    Main menu transition done, and added piano seat

--- Assets/Editor_Tools/Rename_Tool/Editor/Rename_Tool.cs
+++ Assets/Editor_Tools/Rename_Tool/Editor/Rename_Tool.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b92c33d1d06b504b7cc2f5fa2b30d7ce69fe55ee8d6058778d7bc8409bb8abe9
-size 1815
+oid sha256:6f0eb346ee7b2324ca04394f6080fb0853a32fc4721e30ddb33cd88ed32f61a8
+size 1854
--- Assets/Resources/Scripts/MainMenuClass.cs
+++ Assets/Resources/Scripts/MainMenuClass.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4773896c8b183d840fcf32c532c8c4c2e941f4dfc606d650585b80b7c6015cd
-size 2161
+oid sha256:4efc97cfc24f48209cf352880b0f6c04db52125406662495d34eca03411fdead
+size 1416
