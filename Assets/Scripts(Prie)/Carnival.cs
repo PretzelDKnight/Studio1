7344a7a6320992805911e1838204a44ffaf15cc7
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2020-04-23 08:56:33 +0400

    Fixed up compiler errors from prieance's script, scaled brawler and applied right textures, added damage taking animation

--- Assets/Scripts(Prie)/Carnival.cs
+++ Assets/Scripts(Prie)/Carnival.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:286af46af4b892b480109fd23ccef22b0d7ecc68b86f2984824b686611eb864a
-size 501
+oid sha256:8347d8110ce14b68058a7678414fdd74220bc9c5227b376f9e0372bfba8f50cf
+size 321
